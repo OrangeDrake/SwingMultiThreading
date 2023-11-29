77caa5e69d20fbc44b0bbb8ab1226c514b496048
Petr Fiala <petrfiala108@gmail.com>
2023-11-29 11:11:23 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "SwingMultiThreading"
+
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.Console;
+
+import static javax.swing.SwingUtilities.invokeLater;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame();
+                frame.setBounds(400, 400, 400, 400);
+                JButton btnClick = new  JButton("Klikni na me");
+                frame.add(btnClick);
+                btnClick.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        System.out.println("Hello Swing");
+                    }
+                });
+                frame.setVisible(true);
+            }
+        });
+    }
+}
